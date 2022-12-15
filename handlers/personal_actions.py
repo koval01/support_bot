d94249ab4a3a4a0c382491d6224b179d2e559966
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-12-15 21:10:21 +0200

    set type for handlers

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from aiogram.types import ChatType
+from aiogram.types import ChatType, ContentType
 
 from dispatcher import dp
 import config
@@ -15,7 +15,7 @@ async def send_hello(message: types.Message):
     await message.reply(answers.messages["hello"])
 
 
-@dp.message_handler(chat_type=[ChatType.PRIVATE])
+@dp.message_handler(chat_type=[ChatType.PRIVATE], content_types=ContentType.TEXT)
 @rate_limit(10, 'send_question')
 async def send_question(message: types.Message):
 
@@ -27,6 +27,6 @@ async def send_question(message: types.Message):
     await message.reply(answers.answer % (answers.messages["bad_words"], "Система"))
 
 
-@dp.message_handler(chat_type=[ChatType.SUPERGROUP, ChatType.GROUP])
+@dp.message_handler(chat_type=[ChatType.SUPERGROUP, ChatType.GROUP], content_types=ContentType.TEXT)
 async def send_answer(message: types.Message):
     await GroupLogic(message).send_from_group
